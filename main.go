c3e799277a1c7d78baf851d042f0dee5c9a1f26b
silago <silago.nevermind@gmail.com>
2020-02-05 18:49:38 +0300

    show all headers

--- main.go
+++ main.go
@@ -59,6 +59,12 @@ func (n *Notifier) Handler(writer http.ResponseWriter, r *http.Request) {
 	}
 	//var request_type string = r.Header.Get("HTTP_X_GITHUB_EVENT")
 	var request_type string = os.Getenv("Http_X_Github_Event")
+	for name, values := range r.Header {
+	    // Loop over all values for the name.
+	    for _, value := range values {
+		fmt.Println(name, value)
+	    }
+}
 
 	switch request_type {
 	case push_event_type:
